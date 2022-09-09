70c9af406f8de0bd51fc7a158ce1d7eac890e19f
jwfaye <josephwfaye@gmail.com>
2022-09-09 16:48:50 +0200

    Build and load platform script Ok !

--- build_platform.py
+++ build_platform.py
@@ -11,22 +11,23 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="LiteX Bare Metal AES App on AMP Architecture.")
-    parser.add_argument("--core_0", default='firev', type=str, help="Core 0 name.")
-    parser.add_argument("--core_1", default='femtorv', type=str, help="Core 1 name")
-    parser.add_argument("--build", action="store_true", help="Build the target platform.")
-    parser.add_argument("--build_force", action="store_true", help="Build even if the build directory exists.")
-    parser.add_argument("--mux", default=False, help="Build the SoC with double output UART or shared UART.")
-    parser.add_argument("--shared_ram_size", default=0x100, help="Shared RAM size value.")
-#    parser.add_argument("--sram_size", default=0x4000, help="SRAM size value, for the two cores.")
-#    parser.add_argument("--main_ram_size", default=0x10000, help="Main RAM size value, for the two cores.")
-    parser.add_argument("--build_dir", default='', help="Base output dir.")
-    parser.add_argument("--load", action="store_true", help="Load the code to the board.")
+    parser.add_argument("--core_0",          default='firev',        help="Core 0 name.")
+    parser.add_argument("--core_1",          default='femtorv',      help="Core 1 name")
+    parser.add_argument("--build",           action="store_true",    help="Build the target platform.")
+    parser.add_argument("--build_force",     action="store_true",    help="Build even if the build directory exists.")
+    parser.add_argument("--mux",             default=False,          help="Build the SoC with double output UART or shared UART.")
+    parser.add_argument("--config_file",     default='configs.json', help="config file path.")
+    parser.add_argument("--config",          default='config_1',     help='configuration to choose.')
+    parser.add_argument("--build_dir",       default='',             help="Base output dir.")
+    parser.add_argument("--load",            action="store_true",    help="Load the code to the board.")
     args = parser.parse_args()
-
+    #
     cwd = os.getcwd()
-
+    #
     if not args.build_dir:
         args.build_dir = os.path.join(cwd, 'build')
+    else:
+        args.build_dir = os.path.join(cwd, args.build_dir)
     if args.core_0 != args.core_1:
         archi = "Heterogen"
         soc_path = f"ext_libs/Asymetric-Multi-Processing/Anisotrop/Dual_Core/{archi}/Fire_Femto"
@@ -44,27 +45,29 @@ def main():
                 print("MAKE_AND_BUILD_INFO : Build already exists and will be deleted.")
                 shutil.rmtree(args.build_dir)
             os.chdir(os.path.join(cwd, soc_path))
-            os.system(f"./ridope_soc.py --build --mux {args.mux} --build_dir {args.build_dir} "
-#                      f"--sram_size {args.sram_size} --main_ram_size {args.main_ram_size} "
-                      f"--shared_ram_size {args.shared_ram_size}")
+            os.system(f"./ridope_soc.py --config_file {args.config_file} --config {args.config}  "
+                      f"--mux {args.mux} --build_dir {args.build_dir} --build ")
             os.chdir(cwd)
         else:
             if os.path.isdir(args.build_dir):
-                pass
-            else:
                 os.chdir(os.path.join(cwd, soc_path))
-                os.system(
-                    f"./ridope_soc.py --build --mux {args.mux} --build_dir {args.build_dir} "
-#                    f"--sram_size {args.sram_size}  "
-#                    f"--main_ram_size {args.main_ram_size} "
-                    f"--shared_ram_size {args.shared_ram_size}")
+                os.system(f"./ridope_soc.py --config_file {args.config_file} --config {args.config}  "
+                          f"--mux {args.mux} --build_dir {args.build_dir} --build ")
+                os.chdir(cwd)
+            else:
+                os.mkdir(args.build_dir)
+                os.chdir(os.path.join(cwd, soc_path))
+                os.system(f"./ridope_soc.py --config_file {args.config_file} --config {args.config}  "
+                          f"--mux {args.mux} --build_dir {args.build_dir} --build ")
                 os.chdir(cwd)
 
-        # Load Platform
-        if args.load:
-            os.chdir(os.path.join(cwd, soc_path))
-            os.system(f"./ridope_soc.py --load")
-            os.chdir(cwd)
+    # Load Platform
+    if args.load:
+        print("We're loading baby !")
+        os.chdir(os.path.join(cwd, soc_path))
+        os.system(f"./ridope_soc.py --config_file {args.config_file} --config {args.config}  "
+                f"--mux {args.mux} --build_dir {args.build_dir} --load ")
+        os.chdir(cwd)
 
 
 if __name__ == "__main__":
